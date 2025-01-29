06983256d0f463be6b7ca31e442e31e5d6567467
Hariprasathrr <hariprasathcreator@gmail.com>
2025-01-29 05:35:46 +0530

    Feat: Helloworld

---
+++ Day 1/FirstProgramOne.java
@@ -0,0 +1,17 @@
+public class FirstProgramOne{
+
+public static void main(String args[]){
+
+System.out.print("Hello World!");
+System.out.print("Hello World!");
+
+}
+}
+
+
+
+public class FirstProgram{
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
