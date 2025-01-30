348c6d765c49884b82991035911d9ed7b7e4f4a3
Hariprasathrr <hariprasathcreator@gmail.com>
2025-01-30 06:44:02 +0530

    Feat : BackwardCompatability

---
+++ Day 2 Forward,Backward compatability/BackwardCom.java
@@ -0,0 +1,5 @@
+public class BackwardCom{
+    public static void main(String abc[]){
+        System.out.println("This is Backward Compatibility Java Code Version 18 runs on Version 21");
+    }
+}
