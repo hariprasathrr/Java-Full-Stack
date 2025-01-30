b844f5b095d92afbc5fc412ef2a6ebc08735b6e0
Hariprasathrr <hariprasathcreator@gmail.com>
2025-01-30 06:42:30 +0530

    Feat: MyProfile.java

---
+++ Day 2 Forward,Backward compatability/MyProfile.java
@@ -0,0 +1,10 @@
+public class MyProfile{
+
+public static void main(String arg[]){
+
+System.out.println("Name: HarryPotter");
+System.out.println("Place:USA");
+System.out.println("Education: BE");
+System.out.println("Learning: Java FSD");
+}
+}
