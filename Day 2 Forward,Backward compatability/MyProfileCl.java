c7018af0ab0257e44ac98f4e620f3f0a076cb6ae
Hariprasathrr <hariprasathcreator@gmail.com>
2025-01-30 06:43:13 +0530

    Feat:MyProfileCl.java

---
+++ Day 2 Forward,Backward compatability/MyProfileCl.java
@@ -0,0 +1,14 @@
+public class MyProfileCl{
+
+public static void main(String arg[]){//01234
+
+System.out.println("This program uses the run time parameters");
+
+System.out.println("Name: " + arg[0]);
+System.out.println("Place: " + arg[1]);
+System.out.println("Education:"+ arg[2]);
+System.out.println("Learning:" + arg[3]);
+
+}
+}
+
