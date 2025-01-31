6f0704aeece7439b3701617d02830293fef902e7
Hariprasathrr <hariprasathcreator@gmail.com>
2025-01-31 10:14:18 +0530

    Feat : Datatype code

---
+++ Day 3 Operators/Datatype.java
@@ -0,0 +1,79 @@
+public class Datatype{
+
+public static void main(String args[]){
+
+System.out.println("Datatypes....................");
+ System.out.println();
+
+System.out.println("Byte....................");
+byte no = 10;
+System.out.println("Byte:" + no);
+System.out.println("Byte Min:" + Byte.MIN_VALUE);
+System.out.println("Byte Max:"+ Byte.MAX_VALUE);
+ System.out.println();
+
+
+System.out.println("Short....................");
+short srt = 1000;
+System.out.println("Short:" + srt);
+System.out.println("Short Min:" + Short.MIN_VALUE);
+System.out.println("Short Max:"+ Short.MAX_VALUE);
+ System.out.println();
+
+
+System.out.println("Integer....................");
+int rollNo = 2123456789;
+System.out.println("Integer:" + rollNo);
+System.out.println("Integer Min:" + Integer.MIN_VALUE);
+System.out.println("Integer Max:"+ Integer.MAX_VALUE);
+ System.out.println();
+
+System.out.println("Long....................");
+long mobileno = 93123456789L;
+System.out.println("Long:" + mobileno);
+System.out.println("Long Min:" + Long.MIN_VALUE);
+System.out.println("Long Max:"+ Long.MAX_VALUE);
+ System.out.println();
+
+System.out.println("Character....................");
+char section = 'A';
+System.out.println("Char:" + section);
+System.out.println("Char Min:" + Character.MIN_VALUE);
+System.out.println("Char Max:"+ Character.MAX_VALUE);
+ System.out.println();
+
+System.out.println("String....................");
+String name="Hariprasath";
+String email="hariprasath@gmail.com";
+String course="Java full stack";
+System.out.println("String email:" + email);
+System.out.println("String name:" + name);
+System.out.println("String course:" + course);
+
+ System.out.println();
+
+
+System.out.println("Float....................");
+float Subject= 90.5f;
+System.out.println("Float:" + Subject);
+System.out.println("MinFloat:" + Float.MIN_VALUE);
+System.out.println("MaxFloat:" + Float.MAX_VALUE);
+
+System.out.println();
+
+System.out.println("Double....................");
+double mark= 9022555.4894775d;
+System.out.println("Double:" + mark);
+System.out.println("MinDouble:" + Double.MIN_VALUE);
+System.out.println("MaxDouble:" + Double.MAX_VALUE);
+System.out.println();
+
+ System.out.println("Boolean....................");
+ boolean isAvailable = true;
+System.out.println("Boolean: " + isAvailable);
+System.out.println("Boolean True: " + Boolean.TRUE);
+  System.out.println("Boolean False: " + Boolean.FALSE);
+
+
+}
+}
